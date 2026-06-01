4ce3ab1a10a75acaf995155cbb19a5cd5753bc3a
thehuman-cheese-greater <mood124@manukaumail.com>
2026-06-01 23:04:48 +1200

    Add files via upload

---
+++ up_classes.py
@@ -0,0 +1,78 @@
+
+class User:
+    def __init__(self, username, password, user_id, age ):
+        self.username =  username
+        self.password = password
+        self.user_id = user_id
+        self.age = age
+
+
+
+class Admin(User): #admin will inherit from user
+    def __init__(self, username, password): 
+
+        super().__init__(username, password)
+
+
+    def admin_login_details():  # admin login details (hashed value)
+        # To uderstand how the hashed value was found to store here || (refer to the end of this file - Line 52)
+        admin_dict = {"username" : b'$2b$12$Adf4pnqH/1IYRurvlJHh2O.PnAI1iFswxVyBnYL5J8e5s2vlQ6vKq', 
+                      "password" : b'$2b$12$Adf4pnqH/1IYRurvlJHh2OwdN7JOp7/i1MRna7QGzKGtQUPMGtktG'}     
+        return admin_dict["username"], admin_dict["password"]
+
+    def courses_available(**course_details):
+        return dict(**course_details)
+    
+
+
+class Student(User): # student will inherit from user
+    def __init__(self, username, password, gender, dob, phonenumber, email, adress, student_id): 
+
+        super().__init__(username, password, gender)
+        self.gender = gender
+        self.dob = dob
+        self.phonenumber = phonenumber
+        self.email = email
+        self.adress = adress
+        self.student_id = student_id
+
+
+    def student_login_details(): # Student login details (hashed value)
+        st_sign_in_uname = {}
+        st_sign_in_pdw = {}
+        st_sign_in_id = {}
+        
+        st_sign_in_uname.setdefault("d_username", []).extend([b'$2b$12$kIUFejZkuGMdv3W1mSTKVu744oU138BIHCsRVughAtoZUEfVS/G36', b'$2b$12$t/tgzjvkHp4pHJKsTFwzH.gF4WiYZA.mtfA.oqCsfoMvkwLZtPCvq'])
+
+        st_sign_in_pdw.setdefault("d_password", []).extend([b'$2b$12$Adf4pnqH/1IYRurvlJHh2OwBh5hn8riOMmCnn7d5Dt3LSeJNyQkRe', b'$2b$12$t/tgzjvkHp4pHJKsTFwzH.BGSSFja4PA7An287UmteCGYgJnYp/Pe'])
+
+        st_sign_in_id.setdefault("d_id", []).append(1)
+
+        return st_sign_in_uname, st_sign_in_pdw, st_sign_in_id
+
+    def student_courses(**student_course_details):
+        return dict(**student_course_details)
+    
+
+Student.student_login_details()
+
+# #______Geting the hashed value to store in dictionary_________#
+# #generating the salt (it will generate a random string, that will be added with the bytes later to get hashed value)
+# generated_salt = bcrypt.gensalt()  
+
+# #_____For_Admin_____(student is the same method)#
+# #encoding the username
+# new_admin_username = "mark"  #note: value is not real username as this is an example of the method
+# admin_bytes_username = new_admin_username.encode('utf-8')
+
+# # #encoding the password 
+# new_admin_password = "1111"     #note: value is not real password as this is an example of the method
+# admin_bytes_password = new_admin_password.encode('utf-8')
+
+# #hash the admin details (combining the bytes and salt)
+# admin_username_hashed = bcrypt.hashpw(admin_bytes_username, generated_salt) 
+# admin_password_hashed = bcrypt.hashpw(admin_bytes_password, generated_salt)
+
+# print("Admin username:", admin_username_hashed) # Show the Hashed value that will be stored in the login detail dictionaries. 
+
+# print("Admin password:", admin_password_hashed)
---
+++ up_sign.py
@@ -0,0 +1,252 @@
+#tkinter gui
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+
+
+#encryption using bcrypt
+import bcrypt  
+
+#files:
+import adminSMS
+import studentSMS 
+import classes_sms #classes python file 
+
+#database
+import database as db
+db.initialise_db()# Initialise DB and default admin on run
+
+
+
+#_______________username____________________
+def uname_live_validation(*args): 
+    # *args is an unknown number of arguments the function will handle. it's function comes from the (*) so it can be named anything like *items or *characters
+
+    uname_characters = user_name.get() # get the value of the str()
+    
+    if uname_characters != "":
+        usernameEntry.config(bg= "light green") # Turn green if valid  
+        global username
+        username = uname_characters      
+    else:
+        usernameEntry.config(bg= "firebrick2") # red if not valid
+        return
+#_______________username____________________
+
+#python3 sms_login_page.py
+#_______________password____________________
+
+#______password_live_validation_____#
+def pdw_live_validation(*args): 
+
+    characters = user_pdw.get() # get the value of the str()
+    if characters.isdigit() and characters != "":
+        passwordEntry.config(bg= "light green") # Turn green if valid  
+        global idnum
+        idnum = characters      
+    else:
+        passwordEntry.config(bg= "firebrick2") # red if not valid
+        return
+#_______________password____________________
+
+
+def toggle_password():
+    if passwordEntry.cget('show') == '*':
+        passwordEntry.config(show='')
+    else:
+        passwordEntry.config(show='*')
+
+
+def show_sign_in_fields():
+
+    lockout_username.grid_forget()
+    lockout_pdw.grid_forget()
+
+    usernameEntry.grid(row=2, column=1, pady=10, padx=20)
+    passwordEntry.grid(row=3, column=1, pady=10, padx=20)
+    loginButton.grid(row=4, column=1, pady=10)
+
+
+#___________check_info_to_determine_admin_or_client______________
+attempt_lockout = 5
+attempt_counter = 0
+
+def check_if_admin_or_student():
+
+    #Sign in lockout - for security 
+    global attempt_counter
+    attempt_counter += 1
+
+    print(attempt_counter)
+    if attempt_counter == attempt_lockout: # checking the number of times the user has clicked the button for sign in lockout to occur
+
+        usernameEntry.grid_forget()
+        passwordEntry.grid_forget()
+        loginButton.grid_forget()
+
+        lockout_username.grid(row=2, column=1, pady=10, padx=20)
+        lockout_pdw.grid(row=3, column=1, pady=10, padx=20)
+        
+        loginButton.after(300000, show_sign_in_fields) #after 5 minutes run the show field function and reset counter
+        attempt_counter = 0
+
+    elif attempt_counter == 3:
+        messagebox.showwarning("Lockout Reminder", "Warning: You have 2 attempts to left to sign in, before temporary lockout.")
+
+
+    else:
+        a_s_username = usernameEntry.get() # get user input of username entry 
+        a_s_password = passwordEntry.get() # get user input of password entry 
+
+        #Encrypt the password and username to check if it is the same as the stored values
+        a_s_username_bytes = a_s_username.encode('utf-8') #encoding using utf-8 to get the array of bytes
+        a_s_password_bytes = a_s_password.encode('utf-8')
+
+
+        #get the hashed values from classes_sms file
+        ad_uname_hash, ad_pdw_hash,= classes_sms.Admin.admin_login_details() #get the values of admin login details
+        st_uname_hash, st_pdw_hash, st_id = classes_sms.Student.student_login_details() #get the values of student login details
+
+        
+        if bcrypt.checkpw(a_s_username_bytes, ad_uname_hash): # checking if admin username was entered
+            if bcrypt.checkpw(a_s_password_bytes, ad_pdw_hash): # checking if admin password was entered
+                print("Admin Logged in")
+                messagebox.showinfo("Login", "Admin login Successful")
+                
+                window.destroy() #destroy current window, for no overlapping windows
+                attempt_counter = 0
+
+                adminSMS.run()
+        
+            else: # will run if the first conditon if met and the second condition fails. ( user enters username correct and pwd wrong)
+                passwordEntry.config(bg= "firebrick2")
+                messagebox.showinfo("Error", "Please re-enter the password (Please type a number)")
+
+
+
+        for st_d_u_keys, st_list_username in st_uname_hash.items():
+            for username_item in st_list_username:
+                
+                if bcrypt.checkpw(a_s_username_bytes, username_item): # checking if studnet username was entered
+                    
+                    for st_d_pdw_keys, st_list_password in st_pdw_hash.items():
+                        for password_item in st_list_password:
+                    
+                            if bcrypt.checkpw(a_s_password_bytes, password_item): # checking if student password was entered
+                                print("Student Logged in")
+                                messagebox.showinfo("Login", "Student login Successful")
+
+                                window.destroy()
+                                attempt_counter = 0
+
+                                studentSMS.start(st_id, student_name= a_s_username) 
+
+                            else:
+                                passwordEntry.config(bg= "firebrick2")
+                                messagebox.showinfo("Error", "Please re-enter the password (Please type a number)")
+
+               
+        print("please re-type the username")
+        usernameEntry.config(bg= "firebrick2")
+        messagebox.showinfo("Error", "Please re-enter the username")
+
+
+
+#__________GUI_stats_here______________
+window = tk.Tk()
+window.geometry('1280x720+0+0')
+window.resizable(False, False)
+titleLogo = PhotoImage(file="graduation-cap.png")
+window.iconphoto(True,titleLogo)
+
+bgImage = PhotoImage(file='bg_two.png')
+bgLabel = tk.Label(window, image=bgImage)
+bgLabel.place(x=0, y=0)
+
+titleFrame = tk.Frame(window,bg="#BDE5EF",width=1280,height=90,bd=10)
+titleFrame.place(x=0,y=30)
+titleLabel = tk.Label(titleFrame,bg="#BDE5EF",
+                    text='Student Management System',
+                    font=('times new roman', 45, 'bold'))
+titleLabel.place(x=260,y=0)
+
+loginFrame = tk.Frame(window,bg="#D1D1D1")
+loginFrame.place(x=650, y=200)
+
+logoImage = tk.PhotoImage(file='profile.png')
+logoLabel = Label(loginFrame, image=logoImage,bg="#D1D1D1",)
+logoLabel.grid(row=0, column=0, columnspan=3, pady=10)
+
+signInLabel = tk.Label(loginFrame,
+                    text='Sign In',
+                    font=('times new roman', 35, 'bold'),
+                    bg="#D1D1D1")
+signInLabel.grid(row=1, column=0, columnspan=3)
+
+usernameImage = tk.PhotoImage(file='username.png')
+usernameLabel = tk.Label(loginFrame,
+                      image=usernameImage,
+                      text='Username',
+                      compound=LEFT,
+                      font=('times new roman', 20, 'bold'),
+                      bg="#D1D1D1")
+usernameLabel.grid(row=2, column=0, pady=10, padx=20)
+
+
+toggleImage = tk.PhotoImage(file='showpassword.png')
+toggleButton = tk.Button(loginFrame, image=toggleImage,bg="#D1D1D1",command=toggle_password)
+toggleButton.grid(row=3, column=2, pady=10, padx=10)
+
+
+user_pdw = tk.StringVar() #Importing the class and setting it as the value of user_pdw
+user_pdw.trace_add("write", pdw_live_validation) #setting the colour of the password entry box as the result of the validate function 
+
+#___________________Password_Entry_____________________#
+passwordEntry = tk.Entry(loginFrame, text=user_pdw,
+                      font=('times new roman', 20, 'bold'),
+                      bd=5, fg='black', show="*")
+passwordEntry.grid(row=3, column=1, pady=10, padx=20)
+#___________________Password_Entry_____________________#
+
+
+#____img____
+passwordImage = tk.PhotoImage(file='password.png')
+passwordLabel = tk.Label(loginFrame,
+                      image=passwordImage,
+                      text='Password',
+                      compound=LEFT,
+                      font=('times new roman', 20, 'bold'),
+                      bg="#D1D1D1")
+passwordLabel.grid(row=3, column=0, pady=10, padx=20)
+#____img____
+
+
+user_name = tk.StringVar() #Importing the class that will set the colour of the username entry box as the result of the validate function 
+user_name.trace_add("write", uname_live_validation) 
+
+
+#___________________Username_Entry_____________________#
+usernameEntry = tk.Entry(loginFrame, text=user_name,
+                      font=('times new roman', 20, 'bold'), bd=5, fg='black')
+usernameEntry.grid(row=2, column=1, pady=10, padx=20)
+#___________________Username_Entry_____________________#
+
+
+#_________________lockout_______________#
+lockout_username = tk.Label(loginFrame, text= "Sign in Locked for 5 minutes", font=('times new roman', 15, 'bold'), bd=5, fg='black')
+lockout_pdw = tk.Label(loginFrame, text= "Sign in Locked for 5 minutes", font=('times new roman', 15, 'bold'), bd=5, fg='black')
+#_________________lockout_______________#
+
+
+
+loginButton = tk.Button(loginFrame,
+                     text='Login',
+                     font=('times new roman', 14, 'bold'),
+                     width=15, fg='white',
+                     bg='cornflowerblue',
+                     activebackground='cornflowerblue',
+                     activeforeground='white',
+                     cursor='hand2', command=check_if_admin_or_student)
+loginButton.grid(row=4, column=1, pady=10)
+
+window.mainloop()
